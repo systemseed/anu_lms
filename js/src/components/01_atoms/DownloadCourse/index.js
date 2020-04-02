20e45bc6d022d47f6753be9ab20137f6695412a0
Sergey Korzh <ikorgik@gmail.com>
2020-04-02 12:22:22 +0300

    [#172100379] Added prototype.

--- js/src/components/01_atoms/DownloadCourse/index.js
+++ js/src/components/01_atoms/DownloadCourse/index.js
@@ -22,6 +22,39 @@ class DownloadCourse extends React.Component {
       // TODO - replace with API call & handling
       console.log(course);
       window.setTimeout(() => this.setState({ loading: false }), 2000);
+
+      const moduleIds = course.modules.map((module) => {
+        const ids = module.lessons.map((lesson) => lesson.id);
+        ids.push(module.assessment.id);
+        ids.push(module.id);
+        return ids;
+      });
+      console.log(moduleIds);
+      const courseContentIds = moduleIds.flat();
+
+      courseContentIds.map((url) => {
+        const request = new Request(url);
+
+        fetch(request, {mode: 'no-cors'})
+          .then(function (response) {
+            // Don't cache redirects or errors.
+            if (response.ok) {
+              const copy = response.clone();
+
+              caches
+                .open('pwa-main-8.x-1.3-v1')
+                .then(function (cache) {
+                  return cache.put(request, copy);
+                })
+                .catch(function (error) {
+                  console.error("Error: ", error);
+                });
+            }
+          })
+          .catch(function (error) {
+            console.error("PWA: Response not cacheable ", error);
+          });
+      });
     };
 
     return (
@@ -32,7 +65,7 @@ class DownloadCourse extends React.Component {
         onClick={handleDownload}
         disabled={loading}
       >
-        Download course
+        Download course22
 
         {loading &&
         <CircularProgress
