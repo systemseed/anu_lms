e7063652bc241b413d1862f3b9174cd0211f31e8
spleshka <spleshka@gmail.com>
2020-04-27 18:03:32 +0300

    Added offline detection for embedded video block.

--- js/src/components/01_atoms/EmbeddedVideo/index.js
+++ js/src/components/01_atoms/EmbeddedVideo/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React  from 'react';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
+import { Offline, Online } from 'react-detect-offline';
 import ReactPlayer from 'react-player';
 import LessonGrid from '../LessonGrid';
 
@@ -13,20 +15,52 @@ const StyledBox = withStyles(theme => ({
   }
 }))(Box);
 
-const EmbeddedVideo = ({ url }) => (
+const BlockWrapper = ({ children }) => (
   <StyledBox>
     <LessonGrid>
-      {url &&
-      <ReactPlayer
-        url={url}
-        controls
-        playing={false}
-        loop={false}
-        width="100%"
-      />
-      }
+      {children}
     </LessonGrid>
   </StyledBox>
-);
+)
+
+const EmbeddedVideo = ({ url }) => {
+
+  // Is the URL is empty - we obviously can't show the video.
+  if (!url) {
+    return null;
+  }
+
+  // Make sure the URL is playable. Does not include cases like private video
+  // settings.
+  const isValid = ReactPlayer.canPlay(url);
+  if (!isValid) {
+    return (
+      <BlockWrapper>
+        <Typography variant="subtitle1">
+          <em>The lesson contains video, but the link to it either invalid or broken.</em>
+        </Typography>
+      </BlockWrapper>
+    );
+  }
+
+  return (
+    <BlockWrapper>
+      <Online>
+        <ReactPlayer
+          url={url}
+          controls
+          playing={false}
+          loop={false}
+          width="100%"
+        />
+      </Online>
+      <Offline>
+        <Typography variant="subtitle1">
+          <em>The lesson contains video, but it is not available offline.</em>
+        </Typography>
+      </Offline>
+    </BlockWrapper>
+  );
+};
 
 export default EmbeddedVideo;
