6a67ea3450157c9a7d87434fec97e2737c031fbf
Chris Nater <chris@systemseed.com>
2020-05-06 13:54:08 +0200

    [#172171292] Added final, simplified IE11 polyfill.

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,7 @@
+// First entry point for IE11 compatibility.
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { create } from 'jss';
