b320b4931bcd2f09d9efeff5a18ddbe068698f94
Chris Nater <chris@systemseed.com>
2020-09-25 14:37:20 +0200

    [#173285369] Installed react-pdf and added basic handling in Resource (untested).

--- js/src/components/01_atoms/Resource/index.js
+++ js/src/components/01_atoms/Resource/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
+import { Document, Page, pdfjs } from 'react-pdf';
 
 import { withTheme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import Chip from '@material-ui/core/Chip';
+import Dialog from '@material-ui/core/Dialog';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
@@ -15,80 +16,107 @@ import ZoomInIcon from '@material-ui/icons/ZoomIn';
 
 import LessonGrid from '../../06_hocs/LessonGrid';
 
-const Resource = ({ theme, t, file, name, description, ...props }) => (
-  <LessonGrid>
-    <Paper
-     elevation={0}
-     variant="outlined"
-     style={{
-       padding: theme.spacing(2),
-       marginBottom: theme.spacing(3),
-       width: 480,
-     }}
-    >
-      <Box display="flex">
-        <DescriptionOutlinedIcon style={{ color: '#0288d1', fontSize: 34 }} />
+const Resource = ({ theme, t, file, name, description, ...props }) => {
+  const [showDialog, setDialog] = useState(false);
+  const [totalPages, setPages] = useState(-1);
 
-        <Box ml={2} mt={0.5}>
+  const toggleDialog = show => setDialog(show);
+
+  return (
+    <>
+      <LessonGrid>
+        <Paper
+          elevation={0}
+          variant="outlined"
+          style={{
+            padding: theme.spacing(2),
+            marginBottom: theme.spacing(3),
+            width: 480,
+          }}
+        >
           <Box display="flex">
-            <Typography variant="h4">
-              {name}
-            </Typography>
+            <DescriptionOutlinedIcon style={{ color: '#0288d1', fontSize: 34 }} />
 
-            <Box
-              style={{
-                backgroundColor: '#eceff1',
-                color: theme.palette.text.primary,
-                borderRadius: 4,
-                padding: `${theme.spacing(0.5)}px ${theme.spacing(1)}px`,
-              }}
-            >
-              <Typography variant="body2">
-                {`${file ? `.${file.type}` : 'ERROR'}`}
-              </Typography>
+            <Box ml={2} mt={0.5}>
+              <Box display="flex">
+                <Typography variant="h4">{name}</Typography>
+
+                <Box
+                  style={{
+                    backgroundColor: '#eceff1',
+                    color: theme.palette.text.primary,
+                    borderRadius: 4,
+                    padding: `${theme.spacing(0.5)}px ${theme.spacing(1)}px`,
+                  }}
+                >
+                  <Typography variant="body2">{`${file ? `.${file.type}` : 'ERROR'}`}</Typography>
+                </Box>
+              </Box>
+
+              {description && (
+                <Typography
+                  variant="body2"
+                  style={{
+                    marginTop: theme.spacing(1),
+                    color: '#78909c',
+                  }}
+                >
+                  {description}
+                </Typography>
+              )}
+
+              <Box display="flex" mt={2}>
+                <Button
+                  className="secondary"
+                  startIcon={<GetAppIcon />}
+                  href={file ? file.path : '#'}
+                >
+                  {t('Download')}
+                </Button>
+
+                {file && file.type === 'pdf' && (
+                  <Button
+                    className="secondary"
+                    startIcon={<ZoomInIcon />}
+                    style={{ marginLeft: theme.spacing(3) }}
+                    onClick={() => setDialog(true)}
+                  >
+                    {t('Preview')}
+                  </Button>
+                )}
+              </Box>
             </Box>
           </Box>
+        </Paper>
+      </LessonGrid>
 
-          {description && (
-            <Typography
-              variant="body2"
-              style={{
-                marginTop: theme.spacing(1),
-                color: '#78909c',
-              }}
-            >
-              {description}
-            </Typography>
-          )}
-
-          <Box
-            display="flex"
-            mt={2}
+      {file && file.type === 'pdf' && (
+        <Dialog onClose={() => toggleDialog(false)} open={showDialog}>
+          <Document
+            file={file.path}
+            onLoadSuccess={({ numPages }) => setPages(numPages)}
+            options={{
+              cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps`,
+              cMapPacked: true,
+            }}
           >
-            <Button
-              className="secondary"
-              startIcon={<GetAppIcon />}
-              href={file ? file.path : '#'}
-            >
-              {t('Download')}
-            </Button>
-
-            {file && file.type === 'pdf' && (
-              <Button
-                className="secondary"
-                startIcon={<ZoomInIcon />}
-                style={{ marginLeft: theme.spacing(3) }}
-                onClick={() => {/* TODO */}}
-              >
-                {t('Preview')}
-              </Button>
-            )}
-          </Box>
-        </Box>
-      </Box>
-    </Paper>
-  </LessonGrid>
-);
+            {new Array(totalPages).map((el, index) => (
+              <>
+                <Page
+                  key={index}
+                  pageNumber={index + 1}
+                  scale={1}
+                  width={800}
+                  renderAnnotations={false}
+                />
+              </>
+            ))}
+          </Document>
+        </Dialog>
+      )}
+    </>
+  );
+};
 
 Resource.propTypes = {
   file: PropTypes.shape({
