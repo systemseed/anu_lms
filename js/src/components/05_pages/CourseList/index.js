aec36abfb6c449c72446dd4ea00b5c31cda62ae4
Chris Nater <chris@systemseed.com>
2021-01-26 20:13:46 +0100

    [#176655664] Added conditional rendering of filters & categories for course index page with investigative console.log for live demo.

--- js/src/components/05_pages/CourseList/index.js
+++ js/src/components/05_pages/CourseList/index.js
@@ -102,6 +102,10 @@ class CourseList extends React.Component {
     const categories = allCategories.filter(
       (cat1, i, array) => array.findIndex(cat2 => cat1.id === cat2.id) === i
     );
+    const allCoursesHaveCategories = nodes.every(node => {
+      console.log(node.categories);
+      return node.categories;
+    });
 
     return (
       <PageContainer>
@@ -131,52 +135,87 @@ class CourseList extends React.Component {
             />
           )}
 
-          {categories.map(category => (
+          {allCoursesHaveCategories ? (categories.map(category => (
             (activeCategory === 'all' || activeCategory === category.id) && (
-            <div style={{ marginBottom: 100 }}>
-              <Accented>
-                <Typography variant="h4" component="h2">
-                  {category.name}
-                </Typography>
-              </Accented>
+              <div style={{ marginBottom: 100 }}>
+                <Accented>
+                  <Typography variant="h4" component="h2">
+                    {category.name}
+                  </Typography>
+                </Accented>
 
-              <StyledGridContainer container spacing={4}>
-                {nodes.map(node => (
-                  node.categories.find(filterCat => category.id === filterCat.id) && (
-                    <Grid item md={4} sm={6} xs={12} key={node.id}>
-                      <StyledCard>
-                        <StyledCardActionArea component="div">
-                          <StyledLink
-                            href={`${getLangCodePrefix()}${node.path}?category=${category.id}`}
-                          >
-                            <Box position="relative">
-                              <Box position="absolute" m={1} style={{ right: 0 }}>
-                                <CourseLabel {...node.label} />
+                <StyledGridContainer container spacing={4}>
+                  {nodes.map(node => (
+                    node.categories.find(filterCat => category.id === filterCat.id) && (
+                      <Grid item md={4} sm={6} xs={12} key={node.id}>
+                        <StyledCard>
+                          <StyledCardActionArea component="div">
+                            <StyledLink
+                              href={`${getLangCodePrefix()}${node.path}?category=${category.id}`}
+                            >
+                              <Box position="relative">
+                                <Box position="absolute" m={1} style={{ right: 0 }}>
+                                  <CourseLabel {...node.label} />
+                                </Box>
+
+                                <StyledCardMedia image={node.image.url} title={node.title} />
                               </Box>
 
-                              <StyledCardMedia image={node.image.url} title={node.title} />
-                            </Box>
+                              <CardContent>
+                                <Typography gutterBottom variant="h4">
+                                  {node.title}
+                                </Typography>
 
-                            <CardContent>
-                              <Typography gutterBottom variant="h4">
-                                {node.title}
-                              </Typography>
+                                <Typography
+                                  variant="body2"
+                                  color="textSecondary"
+                                  dangerouslySetInnerHTML={{ __html: node.description }}
+                                />
+                              </CardContent>
+                            </StyledLink>
+                          </StyledCardActionArea>
+                        </StyledCard>
+                      </Grid>
+                    )
+                  ))}
+                </StyledGridContainer>
+              </div>
+            )
+          ))) : (
+            <StyledGridContainer container spacing={4}>
+              {nodes.map(node => (
+                <Grid item md={4} sm={6} xs={12} key={node.id}>
+                  <StyledCard>
+                    <StyledCardActionArea component="div">
+                      <StyledLink
+                        href={`${getLangCodePrefix()}${node.path}?category=${category.id}`}
+                      >
+                        <Box position="relative">
+                          <Box position="absolute" m={1} style={{ right: 0 }}>
+                            <CourseLabel {...node.label} />
+                          </Box>
 
-                              <Typography
-                                variant="body2"
-                                color="textSecondary"
-                                dangerouslySetInnerHTML={{ __html: node.description }}
-                              />
-                            </CardContent>
-                          </StyledLink>
-                        </StyledCardActionArea>
-                      </StyledCard>
-                    </Grid>
-                  )
-                ))}
-              </StyledGridContainer>
-            </div>
-          )))}
+                          <StyledCardMedia image={node.image.url} title={node.title} />
+                        </Box>
+
+                        <CardContent>
+                          <Typography gutterBottom variant="h4">
+                            {node.title}
+                          </Typography>
+
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            dangerouslySetInnerHTML={{ __html: node.description }}
+                          />
+                        </CardContent>
+                      </StyledLink>
+                    </StyledCardActionArea>
+                  </StyledCard>
+                </Grid>
+              ))}
+            </StyledGridContainer>
+          )}
         </Container>
       </PageContainer>
     );
