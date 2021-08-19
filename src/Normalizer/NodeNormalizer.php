811b0740ec4bc2c0829dd41c5fe8f1621019fa18
Rodrigo <rodrigo@systemseed.com>
2021-08-19 15:54:04 +0200

    Add a progress bar for courses (#106)
    * Add a progress bar for courses
    
    * Consider quizzes as part of progress
    
    * Use undefined to represent progress is disabled

--- src/Normalizer/NodeNormalizer.php
+++ src/Normalizer/NodeNormalizer.php
@@ -3,6 +3,7 @@
 namespace Drupal\anu_lms\Normalizer;
 
 use Drupal\rest_entity_recursive\Normalizer\ContentEntityNormalizer;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
  * Converts the Drupal node object structure to a JSON array structure.
@@ -36,7 +37,68 @@ class NodeNormalizer extends ContentEntityNormalizer {
       $normalized['is_restricted'] = ['value' => $lessonHandler->isRestricted($entity)];
     }
 
+    /** @var \Drupal\anu_lms\Course $courseHandler */
+    $courseHandler = \Drupal::service('anu_lms.course');
+
+    if ($entity->bundle() === 'course' && $courseHandler->isLinearProgressEnabled($entity)) {
+      $normalized['progress'] = ['value' => $this->getCourseProgress($entity)];
+    }
+
     return $normalized;
+  }
+
+  /**
+   * Returns progress in a course.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $course
+   *   Course object.
+   *
+   * @return float
+   *   The progress as a percentage like 33.34.
+   */
+  protected function getCourseProgress(EntityInterface $course) {
+    /** @var \Drupal\anu_lms\Lesson $lessonHandler */
+    $lessonHandler = \Drupal::service('anu_lms.lesson');
+
+    $modules = $course->get('field_course_module')->referencedEntities();
+    $totalLessons = 0;
+    $completedLessons = 0;
+    foreach ($modules as $module) {
+
+      /** @var \Drupal\node\NodeInterface[] $lessons */
+      $lessons = $module->field_module_lessons->referencedEntities();
+      foreach ($lessons as $lesson) {
+        if ($lesson->access('view')) {
+          $totalLessons++;
+          if ($lessonHandler->isCompleted($lesson)) {
+            $completedLessons++;
+          }
+        }
+      }
+      if (!$module->field_module_assessment) {
+        continue;
+      }
+      /** @var \Drupal\node\NodeInterface[] $quizzes */
+      $quizzes = $module->field_module_assessment->referencedEntities();
+      foreach ($quizzes as $quiz) {
+        if ($quiz->access('view')) {
+          $totalLessons++;
+          if ($lessonHandler->isCompleted($quiz)) {
+            $completedLessons++;
+          }
+        }
+      }
+    }
+
+    // Calculate percentage.
+    if ($totalLessons > 0) {
+      $progress = round($completedLessons * 100 / $totalLessons, 2);
+    }
+    else {
+      $progress = 0;
+    }
+
+    return $progress;
   }
 
 }
