bb0b528d098465887ceeb01a2629b9f31a41f309
Emma <emmab@systemseed.com>
2021-10-18 12:12:12 +0200

    Hides heading on small screens

--- js/src/components/Heading.js
+++ js/src/components/Heading.js
@@ -3,13 +3,25 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import LessonGrid from '@anu/components/LessonGrid';
 
-const Heading = ({ type, value }) => (
-  <LessonGrid>
-    <Typography variant={type} component={type}>
-      {value}
-    </Typography>
-  </LessonGrid>
-);
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    display: 'block',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+const Heading = ({ type, value }) => {
+  const classes = useStyles();
+  return (
+    <LessonGrid>
+      <Typography variant={type} component={type} className={classes.heading}>
+        {value}
+      </Typography>
+    </LessonGrid>
+  );
+};
 
 Heading.propTypes = {
   type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
