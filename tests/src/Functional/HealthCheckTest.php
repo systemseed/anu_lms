c92f8564e12f9065507131e23679fe84a721df0a
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2021-11-03 16:09:32 +0100

    Setup CI.

---
+++ tests/src/Functional/HealthCheckTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\Tests\anu_lms\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Verify that the configured defaults load as intended.
+ *
+ * @group anu_lms
+ */
+class HealthCheckTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['anu_lms'];
+
+  /**
+   * Set to TRUE to strict check all configuration saved.
+   *
+   * @var bool
+   *
+   * @see \Drupal\Core\Config\Testing\ConfigSchemaChecker
+   */
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that the admin works.
+   */
+  public function testOpenAdminPage() {
+    $this->drupalGet('admin');
+  }
+
+}
